d09058dafbdb18cbd91316e5b502f0a6eb29f84b
Justin C <js421503@dal.ca>
2016-11-07 22:59:28 -0400

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var webpack = require('webpack');
 module.exports = {
   devtool: 'eval',
   entry: [
-    'webpack-dev-server/client?https://localhost:3000',
+    'webpack-dev-server/',
     './src/index.jsx'
   ],
   output: {
